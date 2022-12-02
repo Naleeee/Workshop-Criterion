d39bb24362fc500fe62e96b542a9f789c6442050
Nathan LEMALE <nthn.lemale@gmail.com>
2022-12-02 11:26:22 +0100

    feat(ex1): Change exercice to exercise

--- Exercice-1/include/exercise1.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int addOrRemoveMoney(int value, int currentMoney);
-int canIBuyAGame(int gamePrice, int currentMoney);
-int thanksGrandma(int currentMoney);
-int canIBuyABread(int myMoney);
